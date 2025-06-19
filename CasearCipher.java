717a32ff5e69d0a8ead2938b1200ddb3213b3cf4
Fleming John <flemjohn08@gmail.com>
2025-06-19 13:01:21 +0530

    Cipher logic

--- CasearCipher.java
+++ CasearCipher.java
@@ -1,1 +1,15 @@
+
+// importing the scanner for getting the input from the user 
 import java.util.Scanner;
+
+// Creating an encrypt method 
+public static String encrypt(String text, int shift) {
+    StringBuilder result = new StringBuilder();
+
+    for (char character : text.toCharArray()) {
+        if (Character.isLetter(character)) {
+            char base = Character.isLowerCase(character) ? 'a' : 'A';
+            
+        }
+    }
+}
